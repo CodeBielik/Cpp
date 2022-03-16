86ca97f7f2d1dcdabca1b6b530217422754c543c
Krystian Bielecki <93045876+CodeBielik@users.noreply.github.com>
2022-03-16 13:57:22 +0100

    Update Rozdział_9_zadania.cpp

--- Rozdział_9_zadania.cpp
+++ Rozdział_9_zadania.cpp
@@ -38,7 +38,7 @@ double Zadanie_4(double x, double y) {
 
 int Zadanie_5(char c) {
     char lower[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\0'};
-    char upper[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '\0' };
+    char upper[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '\0'};
     
     if (isalpha(c) != 0)
     {
